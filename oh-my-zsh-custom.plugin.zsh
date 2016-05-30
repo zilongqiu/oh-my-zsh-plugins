eee9bb2114cb2625dfe315a44780528d59da8c66
Zi-long QIU <zilong.qiu@gmail.com>
2016-05-31 01:56:09 +0200

    Add taildir alias

---
+++ oh-my-zsh-custom.plugin.zsh
@@ -0,0 +1,12 @@
+# GIT aliases
+# DOCKER aliases
+# PHPUNIT aliases
+# OTHERS aliases
+taildir() {
+    if [ "$1" != "" ]
+    then
+        watch "ls -lrt $@ | tail -10"
+    else
+        watch "ls -lrt | tail -10"
+    fi
+}
