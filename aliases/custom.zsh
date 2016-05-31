a9087b721c88257bcc98505166c328db66dffdb3
Zi-long QIU <zilong.qiu@gmail.com>
2016-05-31 20:54:46 +0200

    Add find file by name alias

--- aliases/custom.zsh
+++ aliases/custom.zsh
@@ -1,3 +1,7 @@
+# Taildir
+# usage examples :
+# taildir
+# taildir *error.log
 taildir() {
     if [ "$1" != "" ]
     then
@@ -5,4 +9,12 @@ taildir() {
     else
         watch "ls -lrt | tail -10"
     fi
+}
+
+# Find file by name
+# usage examples :
+# findn . myfile.extension
+# findn / *.extension
+findn () {
+    sudo find "$1" -name "$2"
 }
