920e8ec08d072056541ee14411097cc221ac5b7f
Zi-long QIU <zilong.qiu@gmail.com>
2016-06-02 01:29:46 +0200

    Add git alias

---
+++ aliases/git.zsh
@@ -0,0 +1,14 @@
+# Aliases
+
+alias gs='git status'
+alias gco='git checkout'
+alias gadd='git add -p'
+alias gcreate='git checkout -b'
+alias greset='git reset HEAD --soft'
+alias gback='git reset HEAD~1'
+alias gpush='git push origin $(git_current_branch)'
+alias gpull='git pull origin $(git_current_branch)'
+alias gcommit='git commit -m'
+alias gclean='git clean -fd'
+alias grset='git remote set-url'
+alias gremote='git remote -v'
