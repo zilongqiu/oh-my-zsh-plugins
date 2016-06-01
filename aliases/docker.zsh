629cd108a6b146c039c0c953eb319e9d2742cd55
Zi-long QIU <zilong.qiu@gmail.com>
2016-06-01 22:27:52 +0200

    Add docker run alias

--- aliases/docker.zsh
+++ aliases/docker.zsh
@@ -28,3 +28,26 @@ dbuild() {
         echo "The command 'docker build' expect the tag name"
     fi
 }
+
+###
+# Docker run
+#
+# usage examples :
+#
+# drun php
+# drun 970eaf375dfd
+###
+drun() {
+    if [ $DOCKER_EXIST = false ]
+    then
+        echo "The command 'docker' was not found"
+        return
+    fi
+
+    if [ "$1" != "" ] && [ -x "$DOCKER" ]
+    then
+        $DOCKER run -it "$1" /bin/bash
+    else
+        echo "The command 'docker run' expect the image name/hash"
+    fi
+}
